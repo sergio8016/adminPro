57ab845f697d712eced2c56f47efcb805375d25c
sergio <sergio8016@gmail.com>
2024-04-16 13:57:34 -0500

    added login and register pages

---
+++ src/app/shared/breadcrums/breadcrums.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrums',
+  templateUrl: './breadcrums.component.html',
+  styles: [
+  ]
+})
+export class BreadcrumsComponent {
+
+}
